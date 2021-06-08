86babba1ef83c41d9cc73b3902c9cd31b0b55ab0
1260Tejasree <83529259+1260Tejasree@users.noreply.github.com>
2021-06-08 14:48:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+import pickle
+app = Flask(__name__)
+filepath="E:\Geethanjaliexternship\CO2.pkl"
+model = pickle.load(open(filepath,'rb'))
+@app.route('/')
+def home():
+    return render_template('home.html')
+@app.route('/Prediction',methods=['POST','GET'])
+def prediction():
+    return render_template('indexnew.html')
+@app.route('/Home',methods=['POST','GET'])
+def my_home():
+    return render_template('home.html')
+@app.route('/predict',methods=["POST","GET"])
+def predict():
+    input_feature=[float(x) for x in request.form.values()]
+    features_values=[np.array(input_feature)]
+    feature_name=['Make','Vehicle_Class','Engine_Size','Cylinders','Transmission','Fuel_Consumption','Fuel_Consumption_City(L/100Km)','Fuel_Consumption_Highway(L/100Km)','Fuel_Consumption_Comb(mpg)','CO2_Emissions']
+    x = pd.DataFrame(features_values,columns=feature_name)
+    prediction=model.predict(x)
+    print("Prediction is:",prediction)
+    return render_template("resultnew.html",prediction=prediction[10])
+if __name__ == "__main__":
+
+    app.run(debug = True) 
